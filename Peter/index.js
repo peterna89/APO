5474b1ec8e68f20fc09eaa7e04f894090089c61a
peter na <peterna_89@hotmail.com>
2017-10-02 00:06:10 -0400

    Peter

---
+++ Peter/index.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+    $("#hide").click(function(){
+        $("p").hide();
+    });
+    
+    $("#show").click(function(){
+        $("p").show();
+    });
+
+    $("#hide1").click(function(){
+        $("p3").hide();
+    });
+    
+    $("#show1").click(function(){
+        $("p3").show();
+    });
+    
+    $("#hide2").click(function(){
+        $("p4").hide();
+    });
+    
+    $("#show2").click(function(){
+        $("p4").show();
+    });
+    
+    
+});
+    
